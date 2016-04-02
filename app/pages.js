773f077e5330c65cabc8d26dd34ba614e4db9ad2
Guy Hughes <g@gxg.me>
2016-04-02 18:37:42 -0400

    presentation mode enabled

--- app/pages.js
+++ app/pages.js
@@ -5,7 +5,7 @@ export default function (node_Id) {
     if(errorHandler(node_Id, json)){
       return;
     }
-    sortByKey(json.data.pages,'count');
+    sortByKey(json.data.pages,'hits');
     var $pages = $('#' + node_Id);
     var rootUrl, title, screenshotUrl, page, maxCount, pageJSON;
 
@@ -13,9 +13,11 @@ export default function (node_Id) {
     maxCount = json.data.pages[0].hits;
     rootUrl = "http://decode-2016.pagecloud.io/"
 
-    for (let i = 0; i < pageJSON.length; i++) {
+    for (let i = 0; i < pageJSON.length && i < 5; i++) {
+      console.log("hits"+pageJSON[i].hits)
+      console.log("maxCount:"+maxCount)
       screenshotUrl = rootUrl + getScreenshot(pageJSON[i].name);
-      page = '<div class="pageDiv col-md-2"><div class="pageTitle">' + pageJSON[i].name + '</div><div class="pageScreenshot"><img style="width:' + ((pageJSON[i].hits/maxCount) * 100) + '%;" src="' + screenshotUrl + '"><div class="pageCount">ViewCount: ' + pageJSON[i].hits + '</div></div></>';
+      page = '<div class="pageDiv col-md-2"><div class="pageTitle">' + pageJSON[i].name + '</div><div class="pageScreenshot"><img style="width:' + ((pageJSON[i].hits/maxCount) * 100) + '%;" src="' + screenshotUrl + '"><div class="pageCount">Views: ' + pageJSON[i].hits + '</div></div></>';
       $pages.append(page);
     }
 
