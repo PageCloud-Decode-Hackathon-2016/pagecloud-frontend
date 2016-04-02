0dfc26725dae348f777d368681aeef98a663f396
Guy Hughes <g@gxg.me>
2016-04-02 17:54:11 -0400

    pages route is live

--- app/pages.js
+++ app/pages.js
@@ -10,17 +10,17 @@ export default function (node_Id) {
     var rootUrl, title, screenshotUrl, page, maxCount, pageJSON;
 
     pageJSON = json.data.pages;
-    maxCount = json.data.pages[0].count;
+    maxCount = json.data.pages[0].hits;
     rootUrl = "http://decode-2016.pagecloud.io/"
 
     for (let i = 0; i < pageJSON.length; i++) {
       screenshotUrl = rootUrl + getScreenshot(pageJSON[i].name);
-      page = '<div class="pageDiv col-md-2"><div class="pageTitle">' + pageJSON[i].name + '</div><div class="pageScreenshot"><img style="width:' + ((pageJSON[i].count/maxCount) * 100) + '%;" src="' + screenshotUrl + '"><div class="pageCount">ViewCount: ' + pageJSON[i].count + '</div></div></>';
+      page = '<div class="pageDiv col-md-2"><div class="pageTitle">' + pageJSON[i].name + '</div><div class="pageScreenshot"><img style="width:' + ((pageJSON[i].hits/maxCount) * 100) + '%;" src="' + screenshotUrl + '"><div class="pageCount">ViewCount: ' + pageJSON[i].hits + '</div></div></>';
       $pages.append(page);
     }
 
     function getScreenshot(name) {
-      return "page_thumbnails/" + name + "_small.jpg";
+      return "page_thumbnails" + name + "_small.jpg";
     }
 
   });
